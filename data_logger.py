8082994f3c4d329dca970b92e085eec96c79f2a3
Christopher Ley <info@christopherley.com>
2024-08-06 13:05:00 -0400

    read end of file test works and first widget using qt6

--- data_logger.py
+++ data_logger.py
@@ -6,33 +6,33 @@ import yaml
 import json
 import copy
 import matplotlib.pyplot as plt
+from datetime import datetime
+from pathlib import Path
 
 
 class State:
     # init
-    def __init__(self, device):
+    def __init__(self, device, dir_path: str = "./"):
         self.device = device
         self.acceleration_x = []
         self.acceleration_y = []
         self.acceleration_z = []
         self.callback = FnVoid_VoidP_DataP(self.data_handler)
+        self.file_name = f"{datetime.now().strftime('%d-%m_%H-%M-%S')}_accel.csv"
+        self.file_path = Path(dir_path) / self.file_name
     # callback
 
     def data_handler(self, ctx, data):
         values = copy.deepcopy(parse_value(data, n_elem=1))
-        self.acceleration_x.append(values.x)
-        self.acceleration_y.append(values.y)
-        self.acceleration_z.append(values.z)
-
-    def __len__(self):
-        return len(self.acceleration_x)
+        csv_to_write = f'{datetime.utcnow().strftime("%F %T.%f")[:-3]},{values.x},{values.y},{values.z}\n'
+        with open(self.file_path, 'a') as f:
+            f.write(csv_to_write)
 
 
-states = []
 # connect
 with open("config.yaml", "r") as file:
     config = yaml.safe_load(file)
-metawear_address = config["devices"][0]["mac"]
+metawear_address = config["devices"][1]["mac"]
 try:
     d = MetaWear(metawear_address)
     d.connect()
@@ -43,50 +43,47 @@ try:
 except Exception as e:
     print(e)
     exit(1)
-states.append(State(d))
+s = State(d)
 
 # configure
-for s in states:
-    print("Configuring device")
-    # setup ble
-    libmetawear.mbl_mw_settings_set_connection_parameters(s.device.board, 7.5, 7.5, 0, 6000)
-    sleep(1.5)
-    # setup acc
-    libmetawear.mbl_mw_acc_set_odr(s.device.board, 100.0)
-    libmetawear.mbl_mw_acc_set_range(s.device.board, 16.0)
-    libmetawear.mbl_mw_acc_write_acceleration_config(s.device.board)
-    # get acc and subscribe
-    signal = libmetawear.mbl_mw_acc_get_acceleration_data_signal(s.device.board)
-    libmetawear.mbl_mw_datasignal_subscribe(signal, None, s.callback)
-    # start acc
-    libmetawear.mbl_mw_acc_enable_acceleration_sampling(s.device.board)
-    libmetawear.mbl_mw_acc_start(s.device.board)
+print("Configuring device")
+# setup ble
+libmetawear.mbl_mw_settings_set_connection_parameters(s.device.board, 7.5, 7.5, 0, 6000)
+sleep(1.5)
+# setup acc
+libmetawear.mbl_mw_acc_set_odr(s.device.board, 100.0)
+libmetawear.mbl_mw_acc_set_range(s.device.board, 16.0)
+libmetawear.mbl_mw_acc_write_acceleration_config(s.device.board)
+# get acc and subscribe
+signal = libmetawear.mbl_mw_acc_get_acceleration_data_signal(s.device.board)
+libmetawear.mbl_mw_datasignal_subscribe(signal, None, s.callback)
+# start acc
+libmetawear.mbl_mw_acc_enable_acceleration_sampling(s.device.board)
+libmetawear.mbl_mw_acc_start(s.device.board)
 
 # sleep
-sleep(10.0)
+sleep(20.0)
 
 # tear down
-for s in states:
-    # stop acc
-    libmetawear.mbl_mw_acc_stop(s.device.board)
-    libmetawear.mbl_mw_acc_disable_acceleration_sampling(s.device.board)
-    # unsubscribe
-    signal = libmetawear.mbl_mw_acc_get_acceleration_data_signal(s.device.board)
-    libmetawear.mbl_mw_datasignal_unsubscribe(signal)
-    # disconnect
-    libmetawear.mbl_mw_debug_disconnect(s.device.board)
-    s.device.disconnect()
+libmetawear.mbl_mw_acc_stop(s.device.board)
+libmetawear.mbl_mw_acc_disable_acceleration_sampling(s.device.board)
+# unsubscribe
+signal = libmetawear.mbl_mw_acc_get_acceleration_data_signal(s.device.board)
+libmetawear.mbl_mw_datasignal_unsubscribe(signal)
+# disconnect
+libmetawear.mbl_mw_debug_disconnect(s.device.board)
+s.device.disconnect()
 
-# recap
-print("Total Samples Received")
-for s in states:
-    print("%s -> %d" % (s.device.address, len(s)))
-    plt.figure()
-    plt.plot(s.acceleration_x, label="x")
-    plt.plot(s.acceleration_y, label="y")
-    plt.plot(s.acceleration_z, label="z")
-    plt.legend()
-    plt.show()
+# file_name =
+#
+#
+# # recap
+# plt.figure()
+# plt.plot(s.acceleration_x, label="x")
+# plt.plot(s.acceleration_y, label="y")
+# plt.plot(s.acceleration_z, label="z")
+# plt.legend()
+# plt.show()
 
 
 
---
+++ read_n_line_test.py
@@ -0,0 +1,20 @@
+import os
+
+
+file = '02-08_16-29-25_accel.csv'
+lines_to_buffer = 10
+n = 0
+with open(file, 'rb') as f:
+    try:
+        f.seek(-2, os.SEEK_END)
+        while n < lines_to_buffer:
+            f.seek(-2, os.SEEK_CUR)
+            if f.read(1) == b'\n':
+                n += 1
+    except OSError:
+        f.seek(0)
+    remainder = f.readlines()
+    data = ""
+    for line in remainder:
+        data += line.decode()
+    print(data)
